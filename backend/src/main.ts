075f396865e38c6c623f2c528fb133506363b1bb
Mostafa Shora <mm7326745@gmail.com>
2026-08-23 18:26:16 +0300

    feat: configure NestJS for Vercel deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import dns from 'dns';
 import { ENV } from './config/env.config';
+// import { Express } from 'express';
 
 dns.setServers(['1.1.1.1', '8.8.8.8']);
 
+const server = express();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   app.use(express.json({ limit: '10mb' }));
-
   app.use(
     express.urlencoded({
       extended: true,
@@ -37,9 +40,15 @@ async function bootstrap() {
     }),
   );
 
-  console.log('Server is running on port:', ENV.PORT ?? 3000);
+  await app.init();
 
-  await app.listen(ENV.PORT ?? 3000);
+  if (process.env.NODE_ENV !== 'production') {
+    const port = ENV.PORT ?? 3000;
+    await app.listen(port);
+    console.log(`Server is running on port: ${port}`);
+  }
 }
 
 bootstrap();
+
+export default server;
