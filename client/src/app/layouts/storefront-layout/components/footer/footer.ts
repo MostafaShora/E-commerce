dc4c545b7102c873778d3b18e2fe8062314d0c07
Mostafa Shora <mm7326745@gmail.com>
2026-09-03 12:50:11 +0300

    Edite UI

--- client/src/app/layouts/storefront-layout/components/footer/footer.ts
+++ client/src/app/layouts/storefront-layout/components/footer/footer.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 import { LogoComponent } from '../../../../shared/components/logo/logo';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterLink, LogoComponent],
+  imports: [RouterLink, LogoComponent, MatIconModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  currentYear = new Date().getFullYear();
+}
